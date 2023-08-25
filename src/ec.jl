7255ba4b973a17f18bd7db12f7111edbb3ac1cd5
Jafagervik <jorgenafagervik@gmail.com>
2023-08-25 15:18:13 +0200

    structure should work

--- src/ec.jl
+++ src/ec.jl
@@ -3,13 +3,14 @@ Bitstrings, genetic algorithms and such thngs
 """
 
 import Base.show
-
 using Base.Threads
 using IterTools
 using Random
+using Test
 
 # Constants
-const FINTESS_TARGET = 5
+const POP_SIZE = 1000
+const FITNESS_TARGET = 100
 const MAX_GENERATIONS = 1000
 
 
@@ -45,7 +46,7 @@ function flip_all!(bs::Bitstring)
 end
 
 function flip_random_n!(bs::Bitstring, n::Integer)
-  idxs = randperm(1, 5)[1:n]
+  idxs = randperm(1, FITNESS_TARGET)[1:n]
   for i in idxs
     flip_at!(bs, i)
   end
@@ -71,117 +72,101 @@ end
 pop_size(pop::Population) = pop.size
 
 
-
 # Init
 function generate_random_population(n::Integer)
-  return Population([Bitstring(join(rand(["0", "1"]) for _ in 1:6)) for _ in 1:n], n)
-end
-
-# Selection
-
-"""
-  Select at most n of all the strings in the population
-"""
-function deterministic_select!(p::Population, n::Integer; treshhold::Integer=1)
-  r::Vector{Bitstring} = [e for e in p.pop if ones(e) >= treshhold]
-
-  println(r)
-  println()
-
-  compare_score(lhs::Bitstring, rhs::Bitstring) = ones(lhs) > ones(rhs)
-
-  # Only take largest elements
-  if length(r) > n
-    sort!(r, by=compare_score)
-    r = r[1:n]
-  end
-
-  println(r)
-
-  # Update population
-  p = Population(r, length(r))
-  return nothing
+  return Population([Bitstring(join(rand(["0", "1"]) for _ in 1:FITNESS_TARGET)) for _ in 1:n], n)
 end
 
 # Mutation - Alpha is the small amount of people that should not be mutated
 function mutate!(p::Population; alpha=0.01)
-  Threads.@threads for child in p.pop
-    mutate!(child)
+  idxs = randperm(1, FITNESS_TARGET)[1:POP_SIZE*alpha]
+  for i in idxs
+    mutate!(p, p.pop[i])
   end
 end
 
 # n is amount of bits to flip
-function mutate!(bs::Bitstring, n=2)
-  flip_random_n!(bs, n)
-end
+mutate!(bs::Bitstring, n=2) = flip_random_n!(bs, n)
 
-# mutate n bits randomly
 
 """ 
 Crossover between multiple bitstrings
 """
 
-# Need a sliding window here and to account for both even and odd populations
-# TODO: IMPROVE
-function cross!(p::Population)
-  sliding_windows = windows(p.pop, 2, step=2)
+"""
+    cross two parents two create two children and add them two pop 
+"""
+function cross!(p::Population, i; split::Float16=0.5)
+  half = Integer(FITNESS_TARGET * split)
 
-  r = Vector{Bitstring}(undef, p.size)
+  c1 = Bitstring(join(p.pop[i].data[1:half]) * join(p.pop[i+1].data[half+1]))
+  c2 = Bitstring(join(p.pop[i+1].data[1:half]) * join(p.pop[i].data[half+1]))
 
-  for (p1, p2) in sliding_windows
-    a = cross(p1, p2)
-    push!(r, a)
-    push!(r, a)
-  end
+  append!(p.pop, c1)
+  append!(p.pop, c2)
 
-  p = Population(r, p.size)
-end
-
-function cross(lhs::Bitstring, rhs::Bitstring; split::Float16=0.5)
-  new_s::Integer = length(lhs) * split
-
-  # FIXME: THis is not concatting to strings 
-  return Bitstring(join(lhs.data[1:new_s], rhs.data[new_s:length(rhs)]))
+  return nothing
 end
 
 
 # Fitness
 f(x::Bitstring) = ones(x)
 
-
-# Eval
-evaluate_population(p::Population)::Bool = all(c -> f(c) > FINTESS_TARGET for c in p.pop)
+# Eval - the best solution has all ones
+evaluate_population(p::Population)::Bool = ones(p.pop[1]) == FITNESS_TARGET
 
 
 function print_statistics(p::Population, gen::Integer)
   print("Found solution in $(gen) generations.\nPopulation with size: $(p.size) where first element is $(p.pop[1])")
 end
 
+#function plotstats(xs, ys; title="test")
+#  plot(xs, ys)
+#
+#  xaxis!()
+#  yaxis!()
+#  title!()
+#
+#  savefig("$(title).pdf")
+#end
+
 
 function main()
-  n = 1000
 
   # INIT
-  population = generate_random_population(n)
+  population = generate_random_population(POP_SIZE)
   curr_gen = 1
 
   found_ideal_generation = false
 
+  # Amount of ones
+  ys::Vector{Float64} = []
+
+
   while curr_gen < MAX_GENERATIONS
     # Evaluation based on fitness
-    if evaluate_population(population)
+    let best = evaluate_population(population)
+
+      # Add best value to ys
+    append!(ys, FITNESS_TARGET - best)
+
+    if best 
       found_ideal_generation = true
       break
     end
 
-    # Selection
-    deterministic_select!(population, n)
-
-    # Crossover
-    cross!(population)
+    # Crossover - cross parents to create children
+    for i = 1:2:POP_SIZE
+      cross!(population, i)
+    end
 
     # Mutation
-    mutate!(population)
+    mutate!(population, alpha=0.2)
+
+    # SELECTION
+    sort!(population.pop, p -> ones(p))
+    population.pop[end:-1:1]
+    population.pop = population.pop[1:POP_SIZE]
 
     curr_gen += 1
   end
@@ -189,18 +174,10 @@ function main()
 
   if found_ideal_generation
     print_statistics(population, curr_gen)
+    #plotstats(1:curr_gen, ys)
   else
     print("Better luck next time.")
   end
 end
 
-function test_main()
-  pop = generate_random_population(6)
-
-  println(pop)
-
-  deterministic_select!(pop, 2)
-end
-
-Random.seed!(42)
-test_main()
+main()
